f93cb089b3491ec111f827dafa05b4a4be029523
yukyeong1016 <75253757+yukyeong1016@users.noreply.github.com>
2021-01-08 11:21:25 +0900

    Add files via upload

--- SQL.sql
+++ SQL.sql
@@ -1,120 +1,100 @@
-create table admin(
+create table bookingroomadmin(
 	adminid varchar2(30) PRIMARY KEY,
 	adminpassword varchar2(30) not null,
 	adminaddress varchar2(50)
-)
+);
 
-create table bookingclient(
+create table bookingroomclient(
 	phonenumber varchar2(30) PRIMARY KEY,
 	password varchar2(20) not null,
 	name varchar2(20) not null,
 	address varchar2(40),
-	starttime DATE,
-	endtime DATE,
-	adminid varchar2(30) references admin(adminid)
-	)
+	adminid varchar2(30),
+	paytime number
+	);
 
-	create table seat(
+	create table bookingroomseat(
 	seatnumber number PRIMARY KEY,
 	seatpartition number,
-	adminid varchar2(30) references admin(adminid)
-	)
+	starttime DATE,
+	endtime DATE,
+	adminid varchar2(30)
+	);
 		
-create table pay(
+create table bookingroompay(
 	paynum number primary key,
-	phonenumber varchar(30) references bookingclient(phonenumber),
-	seatnumber number references seat(seatnumber))
+	phonenumber varchar(30), 
+	seatnumber number,
+	paytime number);
 
-create sequence seatnum;
+
+create sequence bookingseatnum;
 	
 	
-insert into seat values(
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 1, null,null, 'admin');
 
-seatnum.nextval, 1, 'admin')
-
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-insert into seat values(
-
-seatnum.nextval, 1, 'admin')
-
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
-insert into seat values(
-
-seatnum.nextval, 0, 'admin')
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
+insert into bookingroomseat values(
+bookingseatnum.nextval, 0, null,null, 'admin');
 
 
-insert into admin values(
+
+
+insert into bookingroomadmin values(
 'admin', 'wkflsdl', 'SMHRD'
-)
+);
 
 
-
-
-
+alter table bookingroomclient add constraint fk_admin_client foreign key(adminid) references bookingroomadmin(adminid);
+alter table bookingroomseat add constraint fk_admin_seat foreign key(adminid) references bookingroomadmin(adminid);
+alter table bookingroompay add constraint fk_phonenumber foreign key(phonenumber) references bookingroomclient(phonenumber);
+alter table bookingroompay add constraint fk_seatnumber foreign key(seatnumber) references bookingroomseat(seatnumber);
 
 
 
